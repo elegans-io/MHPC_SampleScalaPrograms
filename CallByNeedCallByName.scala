452b56a9298fe4d814c630fa2c852454e005ae00
Angelo Leto <angleto@gmail.com>
2016-11-28 18:42:59 +0100

    better explanations

--- CallByNeedCallByName.scala
+++ CallByNeedCallByName.scala
@@ -1,12 +1,17 @@
 object CallByNeedCallByName extends App {
     def evaluateif[A](c: Boolean, x: => A) = {
-      lazy val y = x
-      if (c) {
+      lazy val y = x // memoization
+      val ret = if (c) {
         (y, y)
       } else {
         (x, x) // evaluated twice
       }
+	  ret
     }
 
-    evaluateif(false, 100)
+    val v0 = evaluateif(false, {println("evaluate0") ; 100})
+	println(v0)
+	println("--------------------")
+	val v1 = evaluateif(true, {println("evaluate1") ; 100})
+    println(v1)
 }
--- CallByNeedCallByName_2.scala
+++ CallByNeedCallByName_2.scala
@@ -4,16 +4,17 @@ object CallByNeedCallByName_2 extends App {
         v
     }
 
-    def evaluateif[A](c: Boolean, x: => A => A, v: => A) = { //x: => A <-- call by name
-      lazy val y = x(v) // memoization
-      val z = x(v) // no memoization
+    def evaluateif[A](c: Boolean, x: A => A, v: => A) = { //x: => A <-- call by name
+      lazy val y = x(v) 
+      val z = x(v) 
       if (c) {
-        println(y, y) // y is evaluated only if c is true and only once
+        println("y", y, y) // y is evaluated twice 
       } else {
-        println(z, z) // x is evaluated twice
+        println("z", z, z) // x is evaluated once
       }
     }
 
-    evaluateif(true, f[String], "first execution")
-    evaluateif(false, f[String], "second execution")
+    evaluateif(false, f[String], "first execution")
+	println("--------------------------------")
+    evaluateif(true, f[String], "second execution")
 }
